28d9d9bad276a5f9aa147af271e349b5a04e12cc
Jacob <jts033@uark.edu>
2022-01-29 16:08:22 -0600

    Added files

---
+++ DB_Create_Script/script.py
@@ -0,0 +1,12 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="jts033",
+  password="ooho0ooD",
+  database= "jts033"
+)
+
+mycursor = mydb.cursor()
+mycursor.execute("create TABLE threat(id int, " +
+"threat_description char(255) NOT NULL,PRIMARY KEY(id));")
