128a4769ffb96c79925c2cc1ff9977bc84ed2682
Zachary-Chapman-Uark <Zachapma@uark.edu>
2022-02-04 12:20:29 -0600

    Added view functionality for all 5 parts

---
+++ src/PHP/View/view_threat.php
@@ -0,0 +1,32 @@
+<html>
+    <body>
+        <h3>View threats:</h3>
+
+        <form method="post">
+            <input type="submit" name="button" value="view threats"/>
+        </form>
+        
+
+        <form action="http://www.csce.uark.edu/~zachapma/ACE/src/home.html">
+            <input type="submit" value="Return to Home Page" />
+        </form>
+        <br><br>
+
+    </body>
+</html>
+
+<?php
+if (isset($_POST['submit'])) 
+{
+    
+    $change_dir = '../python/View';
+    $command = 'python3 view_threat.py';
+
+    // remove dangerous characters from command to protect web server
+    $dir_command = escapeshellcmd($change_dir);
+    $escaped_command = escapeshellcmd($command);
+
+    system($dir_command);
+    system($escaped_command);           
+}
+?>
